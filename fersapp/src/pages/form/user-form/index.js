fbec0782d82e4083f0981a0e37975aef8305666b
pooja shyam <pooja.1690@gmail.com>
2019-08-22 21:51:10 -0500

    so far

--- fersapp/src/pages/form/user-form/index.js
+++ fersapp/src/pages/form/user-form/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import TermsAndConditions from "../../terms-conditions";
-import Main from "./main";
-const terms = false;
+import { steps } from "./main";
+import MultiStep from "react-multistep";
+
+const terms = true;
 
 function UserForm() {
-  return <div>{!terms ? <TermsAndConditions /> : <Main />}</div>;
+  return (
+    <div>
+      {!terms ? (
+        <TermsAndConditions />
+      ) : (
+        <div className="container">
+          <div>
+            <MultiStep showNavigation={true} steps={steps} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default UserForm;
